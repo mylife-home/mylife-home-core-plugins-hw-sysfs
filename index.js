ed86bafad87db296816b2570e9618bcdf16087d3
vincent-tr <vincent@stoic.com>
2018-02-01 10:43:12 +0100

    pwm-rgb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   AcButton : require('./lib/ac-button'),
   AcDimmer : require('./lib/ac-dimmer'),
   Pwm      : require('./lib/pwm'),
+  PwmRgb   : require('./lib/pwm-rgb'),
   GpioIn   : require('./lib/gpio-in'),
   GpioOut  : require('./lib/gpio-out')
 };
