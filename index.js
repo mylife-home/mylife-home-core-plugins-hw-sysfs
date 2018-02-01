7fb69d553fe88d7ba8e7ba8a66c8d2793aed16cc
vincent-tr <vincent@stoic.com>
2018-02-01 09:03:12 +0100

    add pwm

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   AcButton : require('./lib/ac-button'),
   AcDimmer : require('./lib/ac-dimmer'),
+  Pwm      : require('./lib/pwm'),
   GpioIn   : require('./lib/gpio-in'),
   GpioOut  : require('./lib/gpio-out')
 };
